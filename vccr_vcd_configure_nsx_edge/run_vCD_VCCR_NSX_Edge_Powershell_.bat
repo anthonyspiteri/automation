34ef60c5d3d684e4b079659db3d2231b80d36062
Anthony Spiteri <anthony@sliema.com.au>
2019-06-13 05:52:21 -0400

    Delete run_vCD_VCCR_NSX_Edge_Powershell_.bat
    Delete batch script

--- vccr_vcd_configure_nsx_edge/run_vCD_VCCR_NSX_Edge_Powershell_.bat
+++
@@ -1,2 +0,0 @@
-﻿cd %SystemRoot%\system32\WindowsPowerShell\v1.0\
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process powershell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\Users\adm.anthony.spiteri\Documents\Terraform\Configure-vCD-VCCR-NSX-ESG_Zettagrid\configure_vCD_VCCR_NSX_Edge.ps1""' -Verb RunAs}"
